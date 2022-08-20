37d3fedf2544d1d22a3138558317ad63f306b44c
canguejamba <canguejamba@gmail.com>
2022-08-20 22:07:19 +0200

    adding eureka server

---
+++ eureka-server/src/main/java/io/server/eureka/EurekaServerApp.java
@@ -0,0 +1,18 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cangue.Jamba
+ * Project name: microservices
+ */
+package io.server.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+}
