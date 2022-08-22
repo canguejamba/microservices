af5c603ded880418b920e75ad518b22104657325
canguejamba <canguejamba@gmail.com>
2022-08-22 02:11:00 +0200

    adding API Gateway service

---
+++ gatewayAPI/src/main/java/io/api/gateway/ApiGatewayApp.java
@@ -0,0 +1,18 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Cangue.Jamba
+ * Project name: microservices
+ */
+package io.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApiGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApp.class, args);
+    }
+}
